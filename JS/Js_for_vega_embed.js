26175d7d017863ed310fd3d16c641ef2bdd0eaac
PrinceAli6 <89121882+PrinceAli6@users.noreply.github.com>
2021-09-17 00:10:37 +1000

    Add files via upload

---
+++ JS/Js_for_vega_embed.js
@@ -0,0 +1,12 @@
+var vg_1 = "JS/Embed_me.json";
+vegaEmbed("#chart", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+
+
+
+
+
+
